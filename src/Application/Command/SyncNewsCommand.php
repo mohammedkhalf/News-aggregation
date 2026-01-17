ad999c441952969d960aa60250129e7be8f18c08
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-01-17 02:29:29 +0200

    Prepare Application layer

--- src/Application/Command/SyncNewsCommand.php
+++ src/Application/Command/SyncNewsCommand.php
@@ -4,5 +4,11 @@ namespace App\Application\Command;
 
 class SyncNewsCommand
 {
+    public function __construct(
+        public readonly ?string $keyword = null,
+        public readonly ?string $language = null,
+        public readonly ?string $fromDate = null,
+        public readonly ?string $toDate = null
+    ) {}
 
 }
--- src/Application/Handler/SyncNewsHandler.php
+++ src/Application/Handler/SyncNewsHandler.php
@@ -2,7 +2,29 @@
 
 namespace App\Application\Handler;
 
+use App\Application\Command\SyncNewsCommand;
+use App\Domain\Article\Repository\ArticleRepositoryInterface;
+use App\Infrastructure\Api\GNewsHttpClient;
+
 class SyncNewsHandler
 {
+    public function __construct(
+        private GNewsHttpClient $client,
+        private ArticleRepositoryInterface $repository
+    ) {}
+
+    public function __invoke(SyncNewsCommand $command): void
+    {
+        foreach ($this->client->fetch($command) as $dto) {
+            $article = $this->repository->findByExternalId($dto['externalId']);
+            if (!$article) {
+                $article = \App\Domain\Article\Article::createFromDto($dto);
+                $this->repository->upsert($article);
+            } else {
+                $article->updateContent($dto);
+                $this->repository->upsert($article);
+            }
+        }
+    }
 
 }
